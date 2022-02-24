1e3c8ea94929fab5e608ba4c8a329d0e457d5b60
AyeMan001 <91797379+AyeMan001@users.noreply.github.com>
2022-02-24 14:51:52 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,44 @@
+import email
+from email import message
+import imp
+from lib2to3.pgen2.token import NEWLINE
+from turtle import pos
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'Sorry Somthing went wrong, Try again..'
+
+def write_to_csv(data):
+    with open('database.csv',newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting =csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
